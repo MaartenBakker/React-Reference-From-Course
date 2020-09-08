a7c95d123751024cbab20c5459b7f9bb166cf042
Maarten Bakker <maartenbakker@gmail.com>
2020-09-08 21:58:03 +0200

    setting up exercise

--- react-project-1/src/AppAsHook.js
+++ react-project-1/src/AppAsHook.js
@@ -24,25 +24,48 @@ const appAsHook = props => {
     });
   };
 
+  const nameChangedHandler = event => {
+    setPersonsState({
+      persons: [
+        { name: 'Maarten', age: 37 },
+        { name: event.target.value, age: 36 },
+        { name: event.target.value, age: 1 },
+      ],
+    });
+  };
+
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer',
+  };
+
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is really working!</p>
-      <button onClick={() => switchNameHandler('Kasper')}>Switch Name</button>
+      <button style={style} onClick={() => switchNameHandler('Kasper')}>
+        Switch Name
+      </button>
       <Person
         name={personState.persons[0].name}
         age={personState.persons[0].age}
         click={switchNameHandler.bind(this, 'Berend')} // using .bind instead of () =>someFunc(). .bind(this, arg) is preferred
+        changed={nameChangedHandler}
       />
       <Person
         name={personState.persons[1].name}
         age={personState.persons[1].age}
+        changed={nameChangedHandler}
       >
         I'm also a Flautist
       </Person>
       <Person
         name={personState.persons[2].name}
         age={personState.persons[2].age}
+        changed={nameChangedHandler}
       />
     </div>
   );
