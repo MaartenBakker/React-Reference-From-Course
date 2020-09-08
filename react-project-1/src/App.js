3dfbb376a10b5b9b83428b1b954f50b764e7d55d
Maarten Bakker <maartenbakker@gmail.com>
2020-09-08 14:59:52 +0200

    update

--- react-project-1/src/App.js
+++ react-project-1/src/App.js
@@ -1,13 +1,30 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a react app</h1>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <Person name="Maarten" age="37" />
+        <Person name="Anna" age="36">
+          I'm also a Flautist
+        </Person>
+        <Person name="Ella" age="3" />
       </div>
     );
+    // Compiles to:
+    // return React.createElement(
+    //   'div',
+    //   null,
+    //   React.createElement(
+    //     'h1',
+    //     { className: 'App' },
+    //     'Hi, I'm a React App'
+    //   )
+    // );
   }
 }
 
